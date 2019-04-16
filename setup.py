2171979259c8a237b0142c5cc0e142ac35539640
atwiinenickson <atwiinenicxon@gmail.com>
2019-04-15 17:43:53 -0700

    Added set up and travis config

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='travistest',
+    packages=['travistest'],
+    version='0.0.7',
+    description='Test project to get acquainted with TravisCI',
+    url='https://github.com/lqdev/TravisTest',
+)
